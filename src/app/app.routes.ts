88f26ca38fdeb212563a4044361c475e21ae1cf8
DarkEdges <nirving.darkedges@darkedges.com>
2025-08-29 17:16:39 +1000

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { Main } from './main/main';
+import { Terms } from './terms/terms';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: Main,
+    },
+    {
+        path: 'terms',
+        component: Terms,
+    },
+];
